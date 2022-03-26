5a0f30e034addefb6792b266c4a2d6ea70a33730
MDShakibul <sakib6610@gmail.com.com>
2022-03-26 15:51:43 +0600

    responsive done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,10 @@ const Cart = () => {
     return (
         <div className='cart'>
             <h3>Selected Place</h3>
+
+            <button className='btn-choosenOne'>Choose One</button>
+            <button className='btn-choosenAgain'>Choose Again</button>
+            
         </div>
     );
 };
